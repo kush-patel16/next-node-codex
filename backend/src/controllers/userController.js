981281f4977d00d6168d44cd6974cef60d817c1c
kushpatel <kushpatel16102003@gmail.com>
2026-02-28 23:49:48 +0530

    add 1 data

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,5 +2,6 @@ export const getUsers = (req, res) => {
   res.json([
     { id: 1, name: "Alice", email: "alice@test.com" },
     { id: 2, name: "Bob", email: "bob@test.com" },
+    { id: 3, name: "Bobby", email: "bobby@test.com" },
   ]);
 };
